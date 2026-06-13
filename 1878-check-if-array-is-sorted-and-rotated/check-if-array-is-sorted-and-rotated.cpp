3211c30455d25250a755194fb68251432b75be52
Tapish <tapish179@gmail.com>
2026-06-14 00:13:59 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (54.45%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        if( is_sorted( nums.begin() , nums.end() ) ) return true;
-
-        for(int x = 0 ; x < nums.size() ; x++){                                // TC = O(N^2)
-            vector<int> B(nums.size() , 0);                                    // SC = O(N)
-
-            for(int i = 0; i < nums.size() ; i++){
-            B[i] = nums[(i+x)%nums.size()];
-            }
-
-            if( is_sorted( B.begin() , B.end() ) ) return true;
+        int count = 0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if( nums[i] > nums[(i+1)%nums.size()] ) count++;
         }
-        return false;
+        return count <= 1 ;
     }
 };
+
+
+
+
+        // if( is_sorted( nums.begin() , nums.end() ) ) return true;
+
+        // for(int x = 0 ; x < nums.size() ; x++){                                // TC = O(N^2)
+        //     vector<int> B(nums.size() , 0);                                    // SC = O(N)
+
+        //     for(int i = 0; i < nums.size() ; i++){
+        //     B[i] = nums[(i+x)%nums.size()];
+        //     }
+
+        //     if( is_sorted( B.begin() , B.end() ) ) return true;
+        // }
+        // return false;
+
+        // 7/10 approach but not efficient as the complexities
