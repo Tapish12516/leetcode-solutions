59a3523ba257b5377b0ed7280b0868db456f5d4a
Tapish <tapish179@gmail.com>
2026-06-14 01:58:06 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (51.62%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i = 0 ;
-        while(i < nums.size()-1){
-            if (nums[i] == nums[i+1]) 
-                nums.erase(nums.begin()+i);
-            else i++;                         // this doesn't allows i to increase when the third element is same  (repetitive ex: 1 1 1)
-        }                                     // TC = O(N) but when worst case when all elements are same then O(N^2)
-        return nums.size() ; 
+    
+    if(nums.empty()) return 0;
+
+    int k = 1;
+    for(int i = 1; i < nums.size(); i++) {
+        if(nums[i] != nums[i-1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
     }
 };
+
+
+    // int i = 0 ;
+    // while(i < nums.size()-1){
+    //     if (nums[i] == nums[i+1]) 
+    //         nums.erase(nums.begin()+i);
+    //     else i++;                         // this doesn't allows i to increase when the third element is same  (repetitive ex: 1 1 1)
+    // }                                     // TC = O(N) but when worst case when all elements are same then O(N^2)
+    // return nums.size() ; 
+
+    // 5/10 approach , fails for the larger size array and 
