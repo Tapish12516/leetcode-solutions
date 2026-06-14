3e04ef1181fdd57e4a6393069379c3746a28c985
Tapish <tapish179@gmail.com>
2026-06-14 23:23:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (20.04%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+
+            if (rev > INT_MAX/10 || 
+               (rev == INT_MAX/10 && digit > 7))
+                return 0;
+
+            if (rev < INT_MIN/10 || 
+               (rev == INT_MIN/10 && digit < -8))
+                return 0;
+
+            rev = rev * 10 + digit;
+            x /= 10;
+        }
+
+        return rev;
+    }
+};
+
+
+
+
+// class Solution {
+// public:
+//     int reverse(int x) {
+//         // int dup = x;
+//         int rev = 0 ;
+//         while(x!=0){
+//             if ( rev > INT_MAX/10 || rev < INT_MIN/10 ) return 0;            //8.5/10
+//             rev = rev*10 + x%10;
+//             x = x/10;
+//         }
+//         return rev;
+//     }
+// };
