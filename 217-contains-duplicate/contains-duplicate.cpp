e7a4f45592fa4e7187982dd8af3cc7b9c561b584
Tapish <tapish179@gmail.com>
2026-06-14 13:07:29 +0530

    Time: 53 ms (37.66%) | Memory: 90.7 MB (62.10%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int , int > mapp;
-        for(int i = 0; i < nums.size() ; i++){
-            mapp[nums[i]]++;
+        unordered_map<int,int> mp;
+
+        for(int num : nums){
+            mp[num]++;
+
+            if(mp[num] > 1)
+                return true;
         }
-        for(int i = 0; i < nums.size() ; i++){
-            if(mapp[nums[i]] >= 2) return true;
-        }
+
         return false;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         unordered_map<int , int > mapp;
+//         for(int i = 0; i < nums.size() ; i++){
+//             mapp[nums[i]]++;
+//         }                                                                    // TC and SC = O(N)
+//         for(int i = 0; i < nums.size() ; i++){                               // build full map and then traverses again 
+//             if(mapp[nums[i]] >= 2) return true;
+//         }
+//         return false;
+//     }
+// };
