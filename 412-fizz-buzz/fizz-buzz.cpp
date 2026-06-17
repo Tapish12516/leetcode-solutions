fcdeee0077378bb88e1442563fc5dbecdbb5055e
Tapish <tapish179@gmail.com>
2026-06-17 22:59:23 +0530

    Time: 2 ms (45.64%) | Memory: 11.8 MB (17.15%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -1,13 +1,38 @@
+
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        vector<string> answer;
-        for(int i = 1 ; i <= n ; i++){
-            if( (i%3 == 0)  &&  (i%5 == 0) ) answer.push_back("FizzBuzz");
-            else if (i%3 == 0) answer.push_back("Fizz");
-            else if (i%5 == 0) answer.push_back("Buzz");
-            else answer.push_back(to_string(i));
+        vector <string> ans;
+        int i=1;
+        while(i<=n)
+        {
+            if((i%3==0)&&(i%5==0)){
+                ans.push_back("FizzBuzz");
+
+            }
+            else if(i%3==0){
+                ans.push_back("Fizz");
+            }
+            else if(i%5==0){
+                ans.push_back("Buzz");
+            }
+            else{
+             ans.push_back(to_string(i));            }
+            i++;
         }
-        return answer;
-    }
+ return ans;   }
 };
+
+// class Solution {
+// public:
+//     vector<string> fizzBuzz(int n) {
+//         vector<string> answer;
+//         for(int i = 1 ; i <= n ; i++){
+//             if( (i%15 == 0 ) answer.push_back("FizzBuzz");
+//             else if (i%3 == 0) answer.push_back("Fizz");
+//             else if (i%5 == 0) answer.push_back("Buzz");
+//             else answer.push_back(to_string(i));
+//         }
+//         return answer;
+//     }
+// };
