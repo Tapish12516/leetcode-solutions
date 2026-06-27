2c370e12394492bae3c929931096ca061f1f9d2b
Tapish <tapish179@gmail.com>
2026-06-27 16:55:20 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (42.57%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        for(int i = 0 ; i < nums.size() ; i++){
-            if ( nums[i] == target ) return i;
-            if(nums[i] > target) return i;                                  //can be done in O(N) but interviewer demands log(n) 
+
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] == target)
+                return mid;
+
+            else if(nums[mid] < target)
+                low = mid + 1;
+
+            else
+                high = mid - 1;
         }
-        return nums.size();
+
+        return low;
     }
 };
+
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         for(int i = 0 ; i < nums.size() ; i++){
+//             if ( nums[i] == target ) return i;
+//             if(nums[i] > target) return i;                                  //can be done in O(N) but interviewer demands log(n) 
+//         }
+//         return nums.size();
+//     }
+// };
 
 
 //array is sorted 
 //so we will use binary search to find the targeted index
-// class Solution {
-// public:
-//     int searchInsert(vector<int>& nums, int target) {
-
-//         int low = 0;
-//         int high = nums.size() - 1;
-
-//         while(low <= high){
-//             int mid = low + (high - low)/2;
-
-//             if(nums[mid] == target)
-//                 return mid;
-
-//             else if(nums[mid] < target)
-//                 low = mid + 1;
-
-//             else
-//                 high = mid - 1;
-//         }
-
-//         return low;
-//     }
-// };
