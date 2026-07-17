881a196e24de606531a2f97608379fd1734a26b1
Tapish <tapish179@gmail.com>
2026-07-17 18:34:33 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (11.68%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int c1 = 0 , c2 = 0 , c3 = 0 ;
-        for(auto it:nums){
-            if(it == 0) c1++;
-            else if(it == 1) c2++;
-            else c3++;
-        }
-        int index = 0 ; 
-        while(c1--){
-            nums[index++] = 0;
-        }
-        while(c2--){
-            nums[index++] = 1;
-        }
-        while(c3--){
-            nums[index++] = 2;
+        int n = nums.size();
+        int low = 0 , mid = 0 , high = n-1;
+        while(mid<=high){
+            if(nums[mid] == 0){
+                swap(nums[mid] , nums[low]);                    //optimal approach
+                low++; mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid] , nums[high]);
+                high--;
+            }
         }
     }
 };
+
+
+        // int c1 = 0 , c2 = 0 , c3 = 0 ;
+        // for(auto it:nums){
+        //     if(it == 0) c1++;
+        //     else if(it == 1) c2++;
+        //     else c3++;
+        // }
+        // int index = 0 ;                              //bruteforce solution 
+        // while(c1--){
+        //     nums[index++] = 0;
+        // }
+        // while(c2--){
+        //     nums[index++] = 1;
+        // }
+        // while(c3--){
+        //     nums[index++] = 2;
+        // }
