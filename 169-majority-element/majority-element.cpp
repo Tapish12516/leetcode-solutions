687fc631d2d999381da9ea50b8da3579cb94a080
Tapish <tapish179@gmail.com>
2026-07-18 22:16:21 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (11.13%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int , int> mapp;
-        for(int i = 0 ; i < nums.size() ; i++){
-            mapp[nums[i]]++;
+        int cnt = 0; int el;
+        for(int i = 0 ; i  <nums.size(); i++){
+            if(cnt == 0){
+                cnt = 1;
+                el = nums[i];
+            }
+            else if(el == nums[i]) cnt++;
+            else cnt--;
         }
-        int x = nums.size() / 2;
-        for(int i = 0 ; i < nums.size() ; i++){
-            if( mapp[nums[i]] > x ) return nums[i];
+        int cnt1 = 0;                                               //optimal approach
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == el) {
+                cnt1++;
+            }
         }
-        return nums[0];
+        if (cnt1 > (nums.size() / 2)) {
+            return el;
+        }
+        return -1;
     }
 };
+
+// unordered_map<int , int> mapp;
+//         for(int i = 0 ; i < nums.size() ; i++){
+//             mapp[nums[i]]++;
+//         }
+//         int x = nums.size() / 2;
+//         for(int i = 0 ; i < nums.size() ; i++){
+//             if( mapp[nums[i]] > x ) return nums[i];
+//         }
+//         return nums[0];
