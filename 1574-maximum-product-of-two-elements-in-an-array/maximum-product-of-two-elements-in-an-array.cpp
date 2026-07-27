87cc5d6e911ae7cf9e4823eec9ff3617e14fa068
Tapish <tapish179@gmail.com>
2026-07-27 23:47:13 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (25.65%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -1,14 +1,32 @@
+// class Solution {
+// public:
+//     int maxProduct(vector<int>& nums) {
+//         int n = nums.size();
+//         int maxi = 0;
+//         for(int i = 0 ; i < n-1 ; i++){
+//             for(int j  = i+1 ; j < n ; j++){                        O(n^2)
+//                 int temp = (nums[i]-1)*(nums[j]-1);
+//                 maxi = max(maxi , temp);
+//             }
+//         }
+//         return maxi;
+//     }
+// };
+
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n = nums.size();
-        int maxi = 0;
-        for(int i = 0 ; i < n-1 ; i++){
-            for(int j  = i+1 ; j < n ; j++){
-                int temp = (nums[i]-1)*(nums[j]-1);
-                maxi = max(maxi , temp);
+        int max1 = 0, max2 = 0;
+
+        for(int num : nums){
+            if(num > max1){
+                max2 = max1;
+                max1 = num;
+            } else if(num > max2){
+                max2 = num;
             }
         }
-        return maxi;
+
+        return (max1 - 1) * (max2 - 1);
     }
 };
