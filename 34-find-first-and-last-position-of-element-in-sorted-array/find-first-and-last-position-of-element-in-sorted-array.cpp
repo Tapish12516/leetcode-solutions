48daeb17609293a1170347c71e093bf84a7f389c
Tapish <tapish179@gmail.com>
2026-08-23 00:45:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (19.17%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-
     int firstOccurrence(vector<int>& nums, int target) {
         int low = 0;
         int high = nums.size() - 1;
         int ans = -1;
-
         while (low <= high) {
             int mid = low + (high - low) / 2;
-
             if (nums[mid] == target) {
                 ans = mid;
                 high = mid - 1;
             }
-            else if (nums[mid] > target) {
-                high = mid - 1;
-            }
-            else {
-                low = mid + 1;
-            }
+            else if (nums[mid] > target)  high = mid - 1;
+            else  low = mid + 1;
         }
-
         return ans;
     }
 
@@ -28,22 +20,15 @@ public:
         int low = 0;
         int high = nums.size() - 1;
         int ans = -1;
-
         while (low <= high) {
             int mid = low + (high - low) / 2;
-
             if (nums[mid] == target) {
                 ans = mid;
                 low = mid + 1;
             }
-            else if (nums[mid] > target) {
-                high = mid - 1;
-            }
-            else {
-                low = mid + 1;
-            }
+            else if (nums[mid] > target)  high = mid - 1;
+            else  low = mid + 1;
         }
-
         return ans;
     }
 
