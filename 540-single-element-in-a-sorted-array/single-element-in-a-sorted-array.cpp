729c9ee75cbda5c9710e8ce01ec1c80441cab692
Tapish <tapish179@gmail.com>
2026-08-23 23:52:49 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (70.15%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -4,16 +4,17 @@ public:
         int n = nums.size();
         if(nums.size()==1) return nums.back();
         if(nums[0] != nums[1]) return nums[0];
-        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];            //edge cases
         int low = 0 , high = nums.size()-1;
         int mid;
         while(low <= high){
             mid = low + (high - low)/2;
-            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
-            else if ( nums[mid] == nums[mid^1] ) low =mid+1;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])   //near elements
+                return nums[mid];    
+            else if ( nums[mid] == nums[mid^1] ) 
+                low =mid+1;                                    //if mid = even left half is of no use(all before are in pairs)
             else high = mid-1;
         }
         return -1;
     }
 };
-
