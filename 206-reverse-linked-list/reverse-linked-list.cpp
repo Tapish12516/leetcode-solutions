bcd8408dad32aa3dbf5c0f41eaa3a78b40900d65
Tapish <tapish179@gmail.com>
2026-09-06 21:50:44 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (8.03%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -11,14 +11,25 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* curr = head;
-        ListNode* prev = NULL;
-        while(curr != NULL){
-            ListNode* next1 = curr->next;           //iterative approach ||classic
-            curr->next = prev;
-            prev = curr;
-            curr = next1;
-        }
-        return prev;
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* newhead =  reverseList(head->next);
+        ListNode* front = head->next;                           //recursive approach 
+        front->next = head;
+        head->next = nullptr;
+        return newhead;
     }
 };
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* curr = head;
+//         ListNode* prev = NULL;
+//         while(curr != NULL){
+//             ListNode* next1 = curr->next;           //iterative approach ||classic
+//             curr->next = prev;
+//             prev = curr;
+//             curr = next1;
+//         }
+//         return prev;
+//     }
+// };
