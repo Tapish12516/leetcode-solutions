1abf6c68880d9a5dea0dc5f54c70d98e44da9993
Tapish <tapish179@gmail.com>
2026-09-08 22:16:10 +0530

    Time: 12 ms (41.12%) | Memory: 11.7 MB (97.29%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -8,14 +8,29 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*, bool> mp;
-        ListNode* temp = head;
-        while (temp != NULL) {
-            if (mp[temp] == 1)  return true;
-            mp[temp] = 1;
-            temp = temp->next;
+    bool hasCycle(ListNode *head){
+        //hair tortoise approach        floyd's algo 
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != nullptr && fast->next != nullptr){            //O(N) TC and O(1) SC
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast == slow) return true;
         }
         return false;
     }
 };
+
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         unordered_map<ListNode*, bool> mp;
+//         ListNode* temp = head;
+//         while (temp != NULL) {                                      //O(NLOGN) TC AND O(N) SC 
+//             if (mp[temp] == 1)  return true;
+//             mp[temp] = 1;
+//             temp = temp->next;
+//         }
+//         return false;
+//     }
+// };
