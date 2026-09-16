685e3ad7e9b9158579425f8904ebf86483b9c7f5
Tapish <tapish179@gmail.com>
2026-09-17 00:07:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (85.65%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -10,7 +10,7 @@ public:
             }
             if (!st.empty()) {
                 nge[i] = st.top();
-            }
+            }                                                               //isme 2 arrays banane hai
             st.push(nums2[i]);
         }
         for (int i = 0; i < nums1.size(); i++) {
@@ -24,3 +24,27 @@ public:
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//         unordered_map<int, int> mp;
+//         stack<int> st;
+//         for (int i = nums2.size() - 1; i >= 0; i--) {
+//             while (!st.empty() && st.top() <= nums2[i]) {
+//                 st.pop();
+//             }
+//             if (st.empty())
+//                 mp[nums2[i]] = -1;                               //isme map use ho rha hai 
+//             else
+//                 mp[nums2[i]] = st.top();
+//             st.push(nums2[i]);
+//         }
+//         vector<int> ans;
+//         for (int x : nums1) {
+//             ans.push_back(mp[x]);
+//         }
+//         return ans;
+//     }
+// };
